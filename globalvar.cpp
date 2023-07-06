ff8654c386a618f241ed1ccc6bce819f5da62444
MangataX <135018958+MangataX@users.noreply.github.com>
2023-07-06 09:13:33 +0800

    globalvar.cpp

--- globalvar.cpp
+++ globalvar.cpp
@@ -1,16 +1,29 @@
-#include "item.h"
+
 #include "globalvar.h"
 int bgmSound=50, toolSound=50, storySound=50;
 int curScene=0, prevScene=0;
 QSize btnSize(120,60), boxSize(50,50);
-int colorStatus[6]={1,0,1,1,1,0};
-int use_color=-1;
-Item* items[6];
-
-
+int colorStatus[5]={0};
 globalvar::globalvar()
 {
 
-
 }
+void globalVar::setBgmSound(int value)
+{
+    // 修改bgmSound的值
+    bgmSound = value;
+}
+
+void globalVar::setToolSound(int value)
+{
+    // 修改toolSound的值
+    toolSound = value;
+}
+
+void globalVar::setStorySound(int value)
+{
+    // 修改storySound的值
+    storySound = value;
+}
+
 
